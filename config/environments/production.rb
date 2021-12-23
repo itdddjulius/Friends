ff5752139d82d8a250162d903532c5e9d8344803
itdddjulius <itddd.julius@gmail.com>
2021-12-23 02:16:15 +0000

    JO-THR23DEC2021 - Friends App. Installed and Configured {gem 'devise', '~> 4.8', '>= 4.8.1'}

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # J.O. - THR23DEC2021 - Enabling (gem devise)
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # ==========================================================================
+  
+
 end
